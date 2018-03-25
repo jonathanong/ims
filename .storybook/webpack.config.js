ab6128bcfde6161b4cffee693cabbeb95d423bd7
jongleberry <me@jongleberry.com>
2018-03-24 22:25:32 -0700

    build storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
