63b01e641b2b57be803ba9afe9a2116b4c94cfd2
jongleberry <me@jongleberry.com>
2018-03-26 00:16:19 -0700

    webpack oops

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -55,7 +55,7 @@ module.exports = {
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css'
+      chunkFilename: '[name].[hash].css'
     }),
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
