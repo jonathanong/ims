f7a4d7a8eb6d78f0bbb6e885e906384503ea4060
jongleberry <me@jongleberry.com>
2018-03-30 02:54:05 -0700

    server: remove babel-polyfill test

--- server/app/__tests__/index.js
+++ server/app/__tests__/index.js
@@ -6,13 +6,6 @@ const server = app.listen()
 
 afterAll(() => server.close())
 
-test('GET /assets/babel-polyfill/polyfill.min.js', async () => {
-  await request(server)
-    .get('/assets/babel-polyfill/polyfill.min.js')
-    .expect(200)
-    .expect('Content-Type', /application\/javascript/)
-})
-
 test('GET /assets/rollbar/rollbar.min.js', async () => {
   await request(server)
     .get('/assets/rollbar/rollbar.min.js')
