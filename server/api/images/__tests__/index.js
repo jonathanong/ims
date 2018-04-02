4ea6a7903150ab7c08f9fb49467249337d93b8f5
jongleberry <me@jongleberry.com>
2018-04-01 22:55:32 -0400

    api: add more image CRUD routes

--- server/api/images/__tests__/index.js
+++ server/api/images/__tests__/index.js
@@ -4,10 +4,10 @@ const assert = require('assert')
 
 const app = require('../../..')
 
-const server = app.listen()
-
 // http://png-pixel.com/
 const SAMPLE_IMAGE = Buffer.from('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==', 'base64')
+const server = app.listen()
+let image
 
 afterAll(() => server.close())
 
@@ -21,4 +21,24 @@ test('POST /api/images', async () => {
 
   assert(body)
   assert(body.id)
+  assert.equal('png', body.format)
+
+  image = body
+})
+
+test('GET /api/images', async () => {
+  const { body } = await request(server)
+    .get('/api/images')
+    .expect(200)
+
+  assert(Array.isArray(body))
+  assert(body.some(x => x.id === image.id))
+})
+
+test('GET /api/images/:id', async () => {
+  const { body } = await request(server)
+    .get(`/api/images/${image.id}`)
+    .expect(200)
+
+  assert.equal(body.id, image.id)
 })
--- server/api/images/index.js
+++ server/api/images/index.js
@@ -2,10 +2,22 @@
 const route = require('koa-path-match')()
 
 const { upload } = require('../../controllers/images')
+const {
+  getImages,
+  getImageById
+} = require('../../models/images/crud')
 
 const routes = module.exports = []
 
 routes.push(route('/images')
+  .get(async (ctx) => {
+    ctx.body = await getImages(ctx.query)
+  })
   .post(async (ctx) => {
     ctx.body = await upload(ctx)
+  }))
+
+routes.push(route('/images/:id')
+  .get(async (ctx) => {
+    ctx.body = await getImageById(ctx.params.id)
   }))
