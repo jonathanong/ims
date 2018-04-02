d6cc29007f884508c8482f2ad14aa70648c7e8c0
jongleberry <me@jongleberry.com>
2018-04-02 00:49:02 -0400

    client: hook up the store

--- client/App.js
+++ client/App.js
@@ -1,14 +1,18 @@
 
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { hot } from 'react-hot-loader'
 import React from 'react'
 
 import TopNav from './components/TopNav'
+import store from './store'
 
 export const App = () => (
-  <BrowserRouter>
-    <TopNav />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <TopNav />
+    </BrowserRouter>
+  </Provider>
 )
 
 export default hot(module)(App)
