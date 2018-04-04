ab101c345b98b034920462b139e7a9f9e118422d
jongleberry <me@jongleberry.com>
2018-04-04 05:18:47 -0400

    bin/serve: add debug statements

--- bin/serve.js
+++ bin/serve.js
@@ -1,25 +1,30 @@
 #!/usr/bin/env node
 
+const debug = require('debug')('ims:bin:serve')
 const stoppable = require('stoppable')
 const http = require('http')
 
 const onError = require('../server/lib/on-error')
 const app = require('../server')
 
-const gracefulShutdownPeriodSeconds = 10
+const gracefulShutdownPeriodSeconds = ~~process.env.GRACEFUL_SHUTDOWN_PERIOD_SECONDS || 10
 const port = process.env.PORT || 3690
 let closing = false
+
+debug('graceful shutdown period in seconds: %d', gracefulShutdownPeriodSeconds)
 
 const server = http.createServer(app.callback())
 stoppable(server, gracefulShutdownPeriodSeconds * 1000)
 
 server.listen(port, function (err) {
   if (err) {
+    debug('server errored')
     console.error(err.stack)
     process.exitCode = 1
     return
   }
 
+  debug('server listening')
   console.log('IMS serving at http://localhost:%s', this.address().port)
 })
 
@@ -27,6 +32,7 @@ server.listen(port, function (err) {
 process.on('unhandledRejection', onError)
 // uncaught exceptions can mess up your code, so restart the server gracefully
 process.on('uncaughtException', (err) => {
+  debug('uncaught exception')
   console.error(err.stack)
   close()
 })
@@ -35,6 +41,7 @@ process.on('SIGINT', close)
 process.on('SIGTERM', close)
 
 function close () {
+  debug('server closing')
   if (closing) return
   closing = true
   server.stop() // from `stoppable`
